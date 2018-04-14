240563fb5d66fb0c97a6d63afadc6eb5e191c792
Max Kernchen <max.f.kernchen@gmail.com>
2018-04-14 19:34:45 -0400

    Typing changes

--- scribbler.js
+++ scribbler.js
@@ -12,16 +12,8 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `scribbler
-            [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
-
-            ###todo for new year dinner party
-
-            - milk
-            - butter
-            - green onion
-            - lots and lots of kiwis 🥝`;
-  var speed = 60;
+  var txt = `./welcome.sh`;
+  var speed = 80;
 
   function typeItOut () {
     if (i < txt.length) {
