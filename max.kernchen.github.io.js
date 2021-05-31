ffe979ff9c5b8827ac4cd18877795f05f43e47eb
maxkernchen <max.f.kernchen@gmail.com>
2021-05-30 22:01:16 -0400

    all contacts now animated, and transform correctly using promises. More work will need to be done to allow for a loading dialog when transformed from terminal.

--- max.kernchen.github.io.js
+++ max.kernchen.github.io.js
@@ -23,33 +23,41 @@ var terminalMessage = 'maxkernchen@maxkernchen.github.io:./welcome.sh <br>' +
   'Usage: <br> Enter 1 - Navigate to Projects, 2 - Navigate to GitHub, ' +
   '3 - Contact Information:';
 
-var typeSpeed = 30;
+  // some booleans if user indvidually clicks on contact info and then loads them again
+  // via terminal or menu link.
+  var contactEmailLoaded = false;
+  var contactPhoneLoaded = false;
+  var contactLinkedInLoaded = false;
+
+  // type speeds for text transformations.
+  var typeSpeed = 30;
+  var slowerTypeSpeed = typeSpeed + 50;
+   // use this wait time to let the user see the text transformation.
+  var slowestTypeSpeed = slowerTypeSpeed + 100;
+
 async function type(){
-
+let i = 0;
 document.getElementById('hidden-input-field').disabled = true;
-
-  var i = 0
-  
-  
-
- for(i;i < terminalMessageTyping.length;i++){
+ for(;i < terminalMessageTyping.length;i++){
  
     // need to add full break tag at once otherwise it won't be interpreted.
     if(terminalMessageTyping.charAt(i) == '<')        
     {
-      document.getElementsByClassName('terminal-typing')[0].innerHTML += terminalMessageTyping.slice(i,i+5);
+      document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend',
+      terminalMessageTyping.slice(i,i+5));
       i+=4;
     }
     else
     {
-      document.getElementsByClassName('terminal-typing')[0].innerHTML += terminalMessageTyping.charAt(i);
+      document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend',
+      terminalMessageTyping.charAt(i));
     }
     
     await new Promise(r => setTimeout(r, typeSpeed));
 
  }
 
-  if(i>=terminalMessageTyping.length)
+  if(i >= terminalMessageTyping.length)
   {
    
     // allow inputs after typing has finished.
@@ -78,7 +86,7 @@ var timeout = 0;
       }
       else
       {
-        document.getElementsByClassName('terminal-typing')[0].innerHTML += '█';
+        document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend', '█');
       }
       
       
@@ -107,59 +115,92 @@ function typeCommands(){
 }
 // transform email contact to actual email.
 async function emailTransform(event){
-var emailTxt = 'x.f.kernchen@gmail.com';
-//first remove uneeded chars
-var slowerTypeSpeed = typeSpeed + 30;
-var emailElement = document.getElementById('email-contact');
-// replace with anchor for mailto link
-emailElement.innerHTML = 
-'<a id="email-anchor" class="contact--item" href="mailto:max.f.kernchen@gmail.com"></a>';
-var emailAnchor = document.getElementById('email-anchor');
-await new Promise(r => setTimeout(r, slowerTypeSpeed + 100));
-emailAnchor.innerHTML = 'mail';
-await new Promise(r => setTimeout(r, slowerTypeSpeed + 400));
-emailAnchor.innerHTML = 'mai';
-await new Promise(r => setTimeout(r, slowerTypeSpeed + 400));
-emailAnchor.innerHTML = 'ma';
-//type remaining email
-  for(i = 0;i < emailTxt.length;i++){
-      var chartxt = emailTxt.charAt(i);
-      emailAnchor.insertAdjacentHTML('beforeend', emailTxt.charAt(i));
-      await new Promise(r => setTimeout(r, slowerTypeSpeed));
-    }
-  
-  
-  emailElement.removeEventListener('click', emailTransform);
+  if(!contactEmailLoaded){
+    let emailTxt = 'x.f.kernchen@gmail.com';
+    //first remove uneeded chars
+
+    let emailElement = document.getElementById('email-contact');
+    // replace with anchor for mailto link
+    emailElement.innerHTML = 
+    '<a id="email-anchor" class="contact--item" href="mailto:max.f.kernchen@gmail.com"></a>';
+    let emailAnchor = document.getElementById('email-anchor');
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    emailAnchor.innerHTML = 'mail';
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    emailAnchor.innerHTML = 'mai';
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    emailAnchor.innerHTML = 'ma';
+  //type remaining email
+    for(let i = 0;i < emailTxt.length;i++){
+        
+        emailAnchor.insertAdjacentHTML('beforeend', emailTxt.charAt(i));
+        await new Promise(r => setTimeout(r, slowerTypeSpeed));
+      }
+    
+    
+    emailElement.removeEventListener('click', emailTransform);
+    contactEmailLoaded = true;
+    return Promise.resolve('email contact transformed');
+    
+  }
 }
 
-// transform email contact to actual email.
+// transform phone text to actual phone number
 async function phoneTransform(){
-  var phoneTxt = '4-728-6806';
-  //first remove uneeded chars
-  var slowerTypeSpeed = typeSpeed + 200;
-  var phoneElement = document.getElementById('phone-contact')
-  phoneElement.innerHTML = 
-  '<a id="phone-anchor" class="contact--item" href="tel:7047286806"></a>';
+  if(!contactPhoneLoaded){
+    let phoneTxt = '4-728-6806';
+    
+  
+    let phoneElement = document.getElementById('phone-contact')
+    phoneElement.innerHTML = 
+    '<a id="phone-anchor" class="contact--item" href="tel:7047286806"></a>';
 
-  var phoneAnchor = document.getElementById('phone-anchor');
-  await new Promise(r => setTimeout(r, slowerTypeSpeed + 100));
-  phoneAnchor.innerHTML = 'hone';
-  await new Promise(r => setTimeout(r, slowerTypeSpeed));
-  phoneAnchor.innerHTML = 'one';
-  await new Promise(r => setTimeout(r, slowerTypeSpeed ));
-  phoneAnchor.innerHTML = 'oe';
-  await new Promise(r => setTimeout(r, slowerTypeSpeed));
-  phoneAnchor.innerHTML = '0';
-  await new Promise(r => setTimeout(r, slowerTypeSpeed));
-  phoneAnchor.innerHTML = '70';
-  //type remaining email
-    for(i = 0;i < phoneTxt.length;i++){
-      phoneAnchor.insertAdjacentHTML('beforeend', phoneTxt.charAt(i));
-        await new Promise(r => setTimeout(r, slowerTypeSpeed - 100));
+    let phoneAnchor = document.getElementById('phone-anchor');
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    phoneAnchor.innerHTML = 'hone';
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    phoneAnchor.innerHTML = 'one';
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    phoneAnchor.innerHTML = 'oe';
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    phoneAnchor.innerHTML = '0';
+    await new Promise(r => setTimeout(r, slowestTypeSpeed));
+    phoneAnchor.innerHTML = '70';
+    
+    for(let i = 0;i < phoneTxt.length;i++){
+        phoneAnchor.insertAdjacentHTML('beforeend', phoneTxt.charAt(i));
+        await new Promise(r => setTimeout(r, slowerTypeSpeed));
+      }
+    // remove listener as we don't want be clickable again.
+    phoneElement.removeEventListener('click',phoneTransform);
+    contactPhoneLoaded = true;
+   return Promise.resolve('phone contact transformed');
+  }
+}
+// transform linkedin text to full linkedin page.
+async function linkedinTransform(){
+  if(!contactLinkedInLoaded){
+    let linkedinTxt = '.com/in/maxkernchen/'
+    
+
+    let linkedinElement = document.getElementById('linkedin-contact')
+    linkedinElement.innerHTML = 
+    '<a id="linkedin-anchor" class="contact--item"'+
+    'href="https://www.linkedin.com/in/maxkernchen/"></a>'; 
+    // dont need to remove any text here just adding.
+    let linkedinAnchor = document.getElementById('linkedin-anchor');
+    linkedinAnchor.innerHTML = 'linkedin';
+    
+
+    for(let i = 0;i < linkedinTxt.length;i++){
+      linkedinAnchor.insertAdjacentHTML('beforeend', linkedinTxt.charAt(i));
+        await new Promise(r => setTimeout(r, slowerTypeSpeed));
       }
       
-      phoneElement.removeEventListener('click',phoneTransform);
-    
+      linkedinElement.removeEventListener('click',linkedinTransform);
+      contactLinkedInLoaded = true;
+      return Promise.resolve('linkedin contact transformed');
+  }
   }
 
 //in page scrolling for project page
@@ -226,6 +267,22 @@ window.addEventListener('scroll', function () {
     }
   }
 });
+
+//helper method to reset the terminal after user input
+function resetTerminalAfterInput(){
+  //reset message and input
+  document.getElementsByClassName('terminal-typing')[0].innerHTML = terminalMessage;
+  document.getElementById('hidden-input-field').value = '';
+  document.getElementById('hidden-input-field').focus();
+  document.getElementById('hidden-input-field').select();
+
+  // flash cursor again
+  stopFlashing = false;
+  toggle = false;
+  clearTimeout(timeout);
+  flashingCommand();
+}
+
 var input = document.getElementById('hidden-input-field');
 // check for enter command for terminal
 input.addEventListener('keyup',  async function(event) {
@@ -235,33 +292,56 @@ input.addEventListener('keyup',  async function(event) {
     //3 maybe download resume?? TODO
     switch(inputTxt){
       case '1':
-        document.getElementsByClassName('terminal-typing')[0].innerHTML += '<br> Sending to Projects...';
+        document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend', '<br> Sending to Projects...');
         await new Promise(r => setTimeout(r, 500));
         window.location = 'projects.html';        
         break;
       case '2':
-        document.getElementsByClassName('terminal-typing')[0].innerHTML += '<br> Sending to GitHub...';
+        document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend', '<br> Sending to GitHub...');
         await new Promise(r => setTimeout(r, 500));
         window.location = 'https://github.com/maxkernchen'; 
         break;
+      case '3':
+        // check to see if we already loaded all 3 contact elements
+        if(contactPhoneLoaded && contactEmailLoaded && contactLinkedInLoaded){
+          document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend', '<br> Contact Information Already Loaded!');
+        }
+        else{
+          document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend', '<br> Loading Contact Information...');
+          let promise = new Promise(function(resolve, reject) {
+             transformAllContactInfo();
+          }).then(function(result) {
+            console.log(result + 'in term input');
+            resetTerminalAfterInput();
+          });
+        }
+        break;
       default:
-        document.getElementsByClassName('terminal-typing')[0].innerHTML += '<br> Invalid Input!';
+        document.getElementsByClassName('terminal-typing')[0].insertAdjacentHTML('beforeend', '<br> Invalid Input!');
         await new Promise(r => setTimeout(r, 1000));
   }
-
-    //reset message and input
-    document.getElementsByClassName('terminal-typing')[0].innerHTML = terminalMessage;
-    document.getElementById('hidden-input-field').value = '';
-    document.getElementById('hidden-input-field').focus();
-    document.getElementById('hidden-input-field').select();
-  
-    // flash cursor again
-    stopFlashing = false;
-    toggle = false;
-    clearTimeout(timeout);
-    flashingCommand();
-}
+  resetTerminalAfterInput();
+ }
 });
+
+  // use a promise to wait for each function to finish before calling the next
+  // transform all 3 contacts sequentially 
+   async function transformAllContactInfo(){
+    return new Promise(function(resolve, reject) {
+      resolve(emailTransform());    
+    }).then(function(result) {
+      console.log(result);
+      return phoneTransform(); 
+    }).then(function(result){
+      console.log(result);
+      return linkedinTransform();
+    }).then(function(result){
+      console.log(result);
+      return Promise.resolve('transformed all contacts');
+    });
+   
+  }
+
 
 // put focus back in input field when clicking on div for terminal
 document.getElementsByClassName('terminal')[0]
@@ -274,3 +354,7 @@ document.getElementsByClassName('terminal')[0]
 document.getElementById('email-contact').addEventListener('click', emailTransform);
 // phone contact element clicked, change to actual phone number.
 document.getElementById('phone-contact').addEventListener('click', phoneTransform);
+// linkedin contact element clicked, change to full linkedin page.
+document.getElementById('linkedin-contact').addEventListener('click', linkedinTransform);
+// menu item for contact info, will smooth scroll and activate methods to transform contact info
+document.getElementById('contact-menu').addEventListener('click', transformAllContactInfo);
